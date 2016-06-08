51a7fc9661f35ce73e2f9bb078dc9e3905cd9ce8
jhwem82 <jhwem82@gmail.com>
2016-06-08 14:46:16 +0900

    Update vehicle_state.py

--- examples/vehicle_state/vehicle_state.py
+++ examples/vehicle_state/vehicle_state.py
@@ -1,12 +1,3 @@
-"""
-© Copyright 2015-2016, 3D Robotics.
-vehicle_state.py: 
-
-Demonstrates how to get and set vehicle state and parameter information, 
-and how to observe vehicle attribute (state) changes.
-
-Full documentation is provided at http://python.dronekit.io/examples/vehicle_state.html
-"""
 from dronekit import connect, VehicleMode
 import time
 
